95c38c12c1c218ce021dd76e0ea0ffb0702fa0dd
Rohan Drummond <drummond.rohan@gmail.com>
2025-10-22 16:01:02 +1300

    #51 Complete responsive design for individual project page

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -9,13 +9,6 @@ export default function Work() {
     <main className="container mx-auto flex flex-col my-50 px-6">
       <section className="grid grid-cols-1 xl:grid-cols-2 gap-12 md:gap-16">
         <ProjectCard
-          name="Two Cents [WIP]"
-          stack={['TypeScript', 'NoSQL', 'PostgreSQL']}
-          blurb="A financial dashboard designed for couples who want insights into their shared finances e.g. wealth distribution, cash flow monitoring,  financial goal tracking."
-          href="/work/two-cents"
-          image={twoCentsImage}
-        />
-        <ProjectCard
           name="TattGPT"
           stack={['TypeScript', 'C#', 'PostgreSQL', 'GPT', 'DALL·E 3']}
           blurb="An AI-powered tattoo idea generator that lets users quickly generate and save designs, without worrying about the fuss of prompt engineering."
@@ -35,6 +28,13 @@ export default function Work() {
           blurb="A micro CMS for sharing travel memories with family and friends. Users can create and edit posts, manage users, and interact through a built-in commenting system."
           href="/work/arbie-abroad"
           image={arbieAbroadImage}
+        />
+        <ProjectCard
+          name="Two Cents [WIP]"
+          stack={['TypeScript', 'NoSQL', 'PostgreSQL']}
+          blurb="A financial dashboard designed for couples who want insights into their shared finances e.g. wealth distribution, cash flow monitoring,  financial goal tracking."
+          href="/work/two-cents"
+          image={twoCentsImage}
         />
       </section>
     </main>
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -31,23 +31,23 @@ export default function Project({
   videoSrc: string
 }) {
   return (
-    <main className="container mx-auto pt-50">
-      <div className="grid grid-cols-2 gap-y-30 gap-x-20">
-        <section className="flex flex-col justify-between">
+    <main className="container mx-auto mt-40 px-6">
+      <div className="grid grid-cols-1 xl:grid-cols-2 gap-16 md:gap-20 xl:gap-y-24">
+        <section className="flex flex-col justify-between gap-8">
           <h1 className="heading-xl">{name}</h1>
-          <div className="flex flex-row gap-4">
+          <div className="flex flex-row flex-wrap gap-4">
             {badges.map((value, index) => (
               <ProjectBadge key={index} tech={value} />
             ))}
           </div>
         </section>
         <section>
-          <video autoPlay loop muted className="rounded-lg">
+          <video autoPlay loop muted className="rounded-xl">
             <source src={videoSrc} type="video/mp4" />
           </video>
         </section>
-        <section className="flex flex-col justify-between items-start">
-          <div className="flex flex-col ">
+        <section className="flex flex-col justify-between items-start gap-8">
+          <div className="flex flex-col">
             {Object.keys(stack).map((key, index) => (
               <h3 key={index} className="body-lg">
                 {key}: {stack[key]}
@@ -75,9 +75,9 @@ export default function Project({
           </motion.div>
         </section>
         <section>
-          <Card className="flex flex-col justify-center p-8">
+          <Card className="flex flex-col justify-center p-6 md:p-8">
             <div className="flex flex-col gap-6">
-              <h4 className="body">Key features</h4>
+              <h4 className="body-lg">Key features</h4>
               <ul className="flex flex-col gap-2 list-disc list-inside">
                 {features.map((value, index) => (
                   <li
@@ -97,85 +97,12 @@ export default function Project({
               <Image
                 src={image}
                 alt=""
-                className="border py-4 px-6 bg-card rounded-md"
+                className="border p-4 bg-card rounded-md"
               />
             </section>
           ))}
       </div>
       <PageEndOptions buttonText="Back to projects" />
     </main>
-
-    // <main className="container mx-auto flex flex-col mt-30 mb-30 gap-20">
-    //   <section className="flex flex-row">
-    //     <div className="flex flex-col justify-between items-end">
-    //       <h1 className="heading-xl">{name}</h1>
-    //       <div className="flex flex-row gap-4">
-    //         {badges.map((value, index) => (
-    //           <ProjectBadge key={index} tech={value} />
-    //         ))}
-    //       </div>
-    //     </div>
-    //     <video autoPlay loop muted className="w-1/2 rounded-lg">
-    //       <source src="/videos/arbie-abroad-demo.mp4" type="video/mp4" />
-    //     </video>
-    //   </section>
-    //   <section className="flex flex-row gap-20">
-    //     <div className="flex flex-col justify-between items-start gap-10 w-1/2">
-    //       <div className="flex flex-col gap-1">
-    //         {Object.keys(stack).map((key, index) => (
-    //           <h3 key={index} className="sub-heading">
-    //             {key}: <span className="font-medium">{stack[key]}</span>
-    //           </h3>
-    //         ))}
-    //       </div>
-    //       <p className="body-text w-[90%]">{description}</p>
-    //       <motion.div
-    //         whileHover={{
-    //           scale: 1.025,
-    //           transition: { duration: 0.2 },
-    //         }}
-    //         transition={{ duration: 0.1 }}
-    //       >
-    //         <Button className="border hover:bg-background" asChild>
-    //           <a
-    //             href={repo}
-    //             target="_blank"
-    //             className="flex f-row items-center gap-3"
-    //           >
-    //             <p className="sub-text">GitHub</p>
-    //             <ExternalLinkIcon />
-    //           </a>
-    //         </Button>
-    //       </motion.div>
-    //     </div>
-    //     <Card className="flex flex-col justify-center w-1/2 p-8">
-    //       <div className="flex flex-col gap-6">
-    //         <h4 className="body-text">Key features</h4>
-    //         <ul className="flex flex-col gap-2 list-disc list-inside">
-    //           {features.map((value, index) => (
-    //             <li
-    //               key={index}
-    //               className="body-text before:content-[''] before:ml-[-12]"
-    //             >
-    //               {value}
-    //             </li>
-    //           ))}
-    //         </ul>
-    //       </div>
-    //     </Card>
-    //   </section>
-    //   <section className="grid grid-cols-2 gap-8 ">
-    //     {images &&
-    //       images.map((image, index) => (
-    //         <Image
-    //           key={index}
-    //           src={image}
-    //           alt=""
-    //           className="border py-4 px-6 bg-card rounded-md"
-    //         />
-    //       ))}
-    //   </section>
-    //   <PageEndOptions buttonText="Back to projects" />
-    // </main>
   )
 }
