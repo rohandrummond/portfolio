4bc3924be5fd1db5f348e0f88af4377d5884d2ca
Rohan Drummond <drummond.rohan@gmail.com>
2026-01-19 17:15:55 +1300

    Update Tip Tap Toe copy

--- src/app/work/tip-tap-toe/page.tsx
+++ src/app/work/tip-tap-toe/page.tsx
@@ -11,15 +11,15 @@ export default function TipTapToe() {
     <Project
       name="Tip Tap Toe"
       repo="https://github.com/rohandrummond/tiptaptoe"
-      firstParagraph="Tip Tap Toe is a console application built for software engineers and developers who want language-specific practice for improving their typing speed. It includes a custom API integration with Google Gemini, to analyse user keystrokes, and provide tailored code sequences for users to practice their typing."
-      secondParagraph="Tip Tap Toe is a console application built for software engineers and developers who want language-specific practice for improving their typing speed. It includes a custom API integration with Google Gemini, to analyse user keystrokes, and provide tailored code sequences for users to practice their typing."
+      firstParagraph="Tip Tap Toe is a typing practice console app designed specifically for devs. It uses a custom API integration with Google's Gemini model to generate language specific code sequences, and analyses your keystroke patterns to create personalised practice exercises that target your weak points."
+      secondParagraph="The project quickly became a great chance to learn more about C#, and a crash course in working with structured data. It let me explore making HTTP requests, real time keystroke capture and enforcing structured JSON output from LLMs.
+"
       badges={['.NET', 'Gemini']}
       highlights={[
-        'Multi-language support with 5 different languages available for practice.',
-        'Dynamic AI-generated assessments from Google’s Gemini API',
-        'Detailed keystroke logging capturing character, accuracy, and timestamps',
-        'Gemini analysis of JSON keystroke logs for identify weaknesses and tailoring practice.',
-        'Progress insights with WPM calculation and benchmarking',
+        'Structured AI output: Custom Gemini API integration with enforced structured outputs and deserialisation',
+        'Real time keystroke analytics: Stopwatch instance captures keystroke timestamps and accuracy, enabling analysis of slow transitions and error patterns',
+        'Personalised practice: Keystroke JSON data to Gemini, which analyses typing patterns and generates targeted practice sequences',
+        'Modern C# Patterns: Leverages C# 12 features like primary constructors and collection expressions',
       ]}
       videoSrc="/videos/tip-tap-toe-demo.mp4"
     />
